d653991f51e96e49c6cfbff9c436c31b3e905099
wolfiee42 <saifalislam2022@gmail.com>
2024-03-19 12:13:50 +0600

    practiceing basic syntax of TypeScript.

---
+++ Typescript103Prac/basics.ts
@@ -0,0 +1,28 @@
+const myName: string = 'Saif Siraj';
+const myAge: number = 22;
+const isMarried: boolean = false;
+
+const myFriends: string[] = ['ashik', 'ali', 'nahin', 'rayan', 'ashraf'];
+const randomNumber: Array<number> = [12, 32, 22];
+
+let obj: any = { x: 0 };
+
+// obj.foo();
+// obj();
+obj.bar = 100;
+console.log(obj);
+obj = 'hello';
+const n: number = obj;
+console.log(n);
+
+
+let object001: any = {
+    a: 1, b: 2, c: 3
+}
+console.log(object001);
+
+object001.d = 4;
+object001.e = 5;
+console.log(object001);
+
+let myNameIs = 'saif'
