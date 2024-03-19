34e95c23b68a5283ba5417e10f72236bbdfe40c3
wolfiee42 <saifalislam2022@gmail.com>
2024-03-20 00:13:21 +0600

    trying to understand type-assertion in typescript.

--- Typescript103Prac/type-assertion.ts
+++ Typescript103Prac/type-assertion.ts
@@ -3,3 +3,19 @@ let shunaMiya: any;
 // shunaMiya. // getting nothing 
 
 // (shunaMiya as number).  // getting Intellisence 
+
+function kgToGram(kg: string | number): number | string | undefined {
+    if (typeof kg === 'string') {
+        const intGram = parseFloat(kg) * 1000;
+        console.log(`function returned kg in string - > ${kg}kg into gram is ${intGram} gram.`);
+    } else if (typeof kg === 'number') {
+        const intGram = kg * 1000;
+        console.log(`${kg}kg into gram is ${intGram} gram.`);
+    } else {
+        console.log("Please return a valid variable type.");
+        return undefined;
+    }
+};
+
+kgToGram('10') as string;
+kgToGram(50) as number;
