3f41e79f383f05eef088ab5e3697292e747f08e8
wolfiee42 <saifalislam2022@gmail.com>
2024-03-21 14:27:30 +0600

    trying to understand literal type and where should it be in and littlebit of literal interface

--- Typescript103Prac/literal-type.ts
+++ Typescript103Prac/literal-type.ts
@@ -11,3 +11,40 @@ changingString;
 const constantString = "Sup! dog!";
 
 constantString;
+
+function printText(s: string, alighment: "left" | "center" | "right") {
+
+};
+
+printText("hello, world", "right");
+// printText("G'Day, mate", "centre"); 
+// wrong, cz the assigned variable is not from the top three
+
+function compare(a: string, b: string): -1 | 0 | 1 {
+    return a === b ? 0 : a > b ? 1 : -1;
+};
+
+interface Options {
+    width: number;
+}
+
+const configure = (x: Options | 'auto') => {
+
+};
+
+configure({ width: 30 });
+configure('auto');
+// configure('automatic');
+// wrong, cz the assigned variable is not from the interface
+
+
+
+// //////////////////////////////////       literal Interface
+const obj22 = {
+    counter: 0
+};
+
+const someRandomCondition = ''
+if (someRandomCondition) {
+    obj22.counter = 1
+}
